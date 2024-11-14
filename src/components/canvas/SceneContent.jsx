43125c25d404a9fa405e9abe93bca1e1098aaa17
Peter Klinkon <info@kernel-image.net>
2024-11-13 21:20:46 -0500

    added camera movement

--- src/components/canvas/SceneContent.jsx
+++ src/components/canvas/SceneContent.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useSprings, animated } from '@react-spring/three'
+import { useSpring, useSprings, animated } from '@react-spring/three'
 import { useGLTF, PerspectiveCamera } from '@react-three/drei'
 import { useEffect, useRef, useState} from 'react'
 import { MeshStandardMaterial, Vector3 } from 'three'
@@ -8,16 +8,18 @@ import { useThree } from '@react-three/fiber'
 
 
 const SceneContent = () =>  {
-
+  const playerHeight = 1.5
   const boboRef = useRef(null);
   const cameraRef = useRef(null);
-  const gloveRef = useRef(null);
   const floorRef = useRef(null);
   const { raycaster } = useThree();
+
 
   useEffect(() => {
     console.log('scene rendered')
   }, [])
+
+
 
   //loaders
 
@@ -45,12 +47,40 @@ const SceneContent = () =>  {
   // config
   const boxingGlove = playerGltf.scene.children[0] //scene didn't contain a mesh?
   const gloveOrigins = {
-    right: [1, 1, 0.5],
-    left: [-1, 1, 0.5],
+    right: [0.5, -0.5, -0.4],
+    left: [-0.5, -0.5, -0.4],
   }
 
   //animation
 
+  //camera spring
+  const [camPosition, setCamPosition] = useState([0, playerHeight, 1.5]);
+  const [nextCamPosition, setNextCamPosition] = useState(camPosition)
+  const [camSpring, camSpringApi] = useSpring(() => ({
+    from: { position: camPosition},
+    to: { position: nextCamPosition},
+    config: {
+      mass: 1,
+      tension: 200,
+      friction: 10,
+      velocity: .02,
+      precision: 0.4
+    },
+    immediate: false,
+    onRest: () => {
+      const restPos = camSpring.position.get()
+      const currentPos = [restPos[0], playerHeight, restPos[2]] 
+      setCamPosition(currentPos)
+    }
+    
+  }), [camPosition, nextCamPosition])
+
+  useEffect(() => {
+    const cam = cameraRef.current
+    cam.lookAt(boboRef.current.position)
+  }, [camPosition])
+
+  //glove spring
   let target = new Vector3();
   const [leftTarget, setLeftTarget] = useState(gloveOrigins.left);
   const [rightTarget, setRightTarget] = useState(gloveOrigins.right);
@@ -105,7 +135,7 @@ const SceneContent = () =>  {
         setRightTarget(gloveOrigins.right)
       }
       else{
-        console.log("right hand at rest")
+        //console.log("right hand at rest")
       }
       
     } else {
@@ -113,7 +143,7 @@ const SceneContent = () =>  {
         setLeftTarget(gloveOrigins.left)
       }
       else{
-        console.log("left hand at rest")
+        //console.log("left hand at rest")
       }
     }
   }
@@ -157,40 +187,53 @@ const SceneContent = () =>  {
 
   const handleLevelClick = (event) => {
     event.stopPropagation()
-    console.log('level clicked')
+    //console.log('level clicked')
+    if (!punching.right && !punching.left) {
+      target = getRaycastHit(raycaster, event, cameraRef.current, floorRef)
+      if (target) {
+        const nextPos = [target.x, playerHeight, target.z]
+        console.log(`move to ${nextPos}`)
+        setNextCamPosition(nextPos)
+      }
+    }
   }
+
+
 
   return (
     <group>
-    <PerspectiveCamera makeDefault fov={90} position={[0, 1.5, 1.5]} ref={cameraRef}/>
-    {/*Level*/}
-    {levelMeshes.map((obj) => <primitive key={obj.name} object={obj} material={levelMaterial} onClick={handleLevelClick}/>)}
-    <group>
-      {/*Bobo*/}
-      <primitive
-          ref={boboRef}
-          object={bobo}
-          material={bobo.material}
-          onClick={handleBoboClick}
-          position={[0, 0, -2]}
-        />
 
-      {/*Right Glove*/}
-      <animated.instancedMesh
-        args={[boxingGlove.geometry, gloveMaterial, 2]}
-        rotation={[Math.PI * -0.4, Math.PI * 0.8, Math.PI * -.1]}
-        position={springs[1].position}
-        scale={[-0.025, 0.025, 0.025]}
-        ref = {gloveRef}
+    <animated.group position={camSpring.position}>
+      <PerspectiveCamera makeDefault fov={90} ref={cameraRef}>
+          {/*Right Glove*/}
+          <animated.instancedMesh
+            args={[boxingGlove.geometry, gloveMaterial, 2]}
+            rotation={[Math.PI * -0.3, Math.PI * 0.9, Math.PI * -.2]}
+            position={springs[1].position}
+            scale={[-0.025, 0.025, 0.025]}
+          />
+          {/*Left Glove*/}
+          <animated.instancedMesh
+            args={[boxingGlove.geometry, gloveMaterial, 2]}
+            rotation={[Math.PI * -0.5, Math.PI * 0.8, Math.PI * 0.2]}
+            position={springs[0].position}
+            scale={0.025}
+          />
+      </PerspectiveCamera>
+    </animated.group>
+
+    {/*Level*/}
+    {levelMeshes.map((obj) => {const isFloor = obj.name.includes("floor"); return <primitive key={obj.name} object={obj} material={levelMaterial} onClick={isFloor ? handleLevelClick : null} ref={isFloor ? floorRef : null}/>})}
+    
+    {/*Bobo*/}
+    <primitive
+        ref={boboRef}
+        object={bobo}
+        material={bobo.material}
+        onClick={handleBoboClick}
+        position={[0, 0, -2]}
       />
-      {/*Left Glove*/}
-      <animated.instancedMesh
-        args={[boxingGlove.geometry, gloveMaterial, 2]}
-        rotation={[Math.PI * -0.5, Math.PI * 0.8, Math.PI * 0.2]}
-        position={springs[0].position}
-        scale={0.025}
-      />
-    </group>
+
     </group>
   )
 }

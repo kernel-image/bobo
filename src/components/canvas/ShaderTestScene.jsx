3017d1c38ba1bce24bdfa44b251783faeba55d40
Peter Klinkon <info@kernel-image.net>
2024-12-04 16:17:09 -0500

    add shader test scenes

--- src/components/canvas/ShaderTestScene.jsx
+++ src/components/canvas/ShaderTestScene.jsx
@@ -1,7 +1,10 @@
 import { PerspectiveCamera } from '@react-three/drei'
 import { MeshFresnelMaterial } from '@/helpers/shaders/fresnelShader/fresnelShader'
+import { MeshRaysMaterial } from '@/helpers/shaders/raysShader/raysShader'
+import { MeshStripeMaterial } from '@/helpers/shaders/raysShader/stripeShader'
+import { OrbitControls } from '@react-three/drei'
 
-const SceneContent = () => {
+const FresnelTest = () => {
   return (
     <>
       <mesh position={[1, 0, 0]}>
@@ -19,4 +22,58 @@ const SceneContent = () => {
   )
 }
 
-export { SceneContent }
+const TentRays = () => {
+  const rays = 16
+  const color1 = 'red'
+  const color2 = 'blue'
+  return (
+    <>
+      <OrbitControls />
+      <mesh position={[0, 1, 0]}>
+        <coneGeometry args={[2, 1, 32]} />
+        <MeshRaysMaterial rays={rays} color1={color1} color2={color2} />
+      </mesh>
+      <mesh position={[0, 0, 0]}>
+        <cylinderGeometry args={[2, 2, 1, 32]} />
+        <MeshRaysMaterial rays={rays} color1={color1} color2={color2} />
+      </mesh>
+    </>
+  )
+}
+
+const TentStripes = () => {
+  const rays = 16
+  const color1 = 'yellow'
+  const color2 = 'blue'
+  return (
+    <>
+      <OrbitControls />
+      <mesh position={[0, 1, 0]}>
+        <coneGeometry args={[2, 1, 32, 1, true]} />
+        <MeshStripeMaterial rays={rays} color1={color1} color2={color2} />
+      </mesh>
+      <mesh position={[0, 0, 0]}>
+        <cylinderGeometry args={[2, 2, 1, 32, 1, true]} />
+        <MeshStripeMaterial rays={rays} color1={color1} color2={color2} />
+      </mesh>
+    </>
+  )
+}
+
+const TentInside = () => {
+  const rays = 16
+  const color1 = 'yellow'
+  const color2 = 'blue'
+  const cullMode = 1
+  return (
+    <>
+      <OrbitControls />
+      <mesh position={[0, 0, 0]}>
+        <cylinderGeometry args={[1, 1, 1, 32]} attach={'geometry'} />
+        <MeshRaysMaterial rays={rays} color1={color1} color2={color2} side={cullMode} attach='material' />
+      </mesh>
+    </>
+  )
+}
+
+export { FresnelTest, TentRays, TentStripes, TentInside }

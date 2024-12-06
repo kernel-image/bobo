31b4bc243746f6012eb5c7f8ed041dbeadd3e652
Peter Klinkon <info@kernel-image.net>
2024-12-06 18:57:00 -0500

    add shadowed noise shader to scene

--- src/helpers/materials.js
+++ src/helpers/materials.js
@@ -1,7 +1,7 @@
 import { MeshBasicMaterial, MeshPhongMaterial, Color, BackSide } from 'three'
 import { CustomFresnel } from './shaders/fresnelShader/fresnelShader'
 import { CustomRays } from './shaders/raysShader/raysShader'
-import { CustomNoise } from './shaders/noiseShader/noiseShader'
+import { CustomShadowedNoise } from './shaders/shadowedNoiseShader/shadowedNoiseShader'
 
 const levelMaterial = new CustomFresnel({
   colorMain: new Color(0.25, 0.08, 0.087),
@@ -13,15 +13,15 @@ const gloveMaterial = new CustomFresnel({
   colorFresnel: new Color(0.09, 0.0045, 0.08),
   power: 0.5,
 })
-const floorMaterial = new CustomNoise({
-  colorMain: new Color(0.4, 0.4, 0.6),
-  colorNoise: new Color(0.6, 0.6, 0.8),
-  contrast: 0.9,
-  gain: 1.0,
-  level: 0.05,
-  scale: 6,
-  octaves: 4,
-  seed: 7,
+const floorMaterial = new CustomShadowedNoise({
+  uColorMain: new Color(0.4, 0.4, 0.6),
+  uColorNoise: new Color(0.6, 0.6, 0.8),
+  uContrast: 0.9,
+  uGain: 1.0,
+  uLevel: 0.05,
+  uScale: 6,
+  uOctaves: 4,
+  uSeed: 7,
 })
 const testMaterial = new MeshBasicMaterial({ color: 'green', alphaTest: 2 })
 const tentMaterial = new CustomRays({

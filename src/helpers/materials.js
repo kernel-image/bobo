8158f428d11d853a9e311521e34372b01c05fb09
Peter Klinkon <info@kernel-image.net>
2024-12-06 14:09:13 -0500

    add noise shader to to scene *** breaks shadows

--- src/helpers/materials.js
+++ src/helpers/materials.js
@@ -1,6 +1,7 @@
 import { MeshBasicMaterial, MeshPhongMaterial, Color, BackSide } from 'three'
 import { CustomFresnel } from './shaders/fresnelShader/fresnelShader'
 import { CustomRays } from './shaders/raysShader/raysShader'
+import { CustomNoise } from './shaders/noiseShader/noiseShader'
 
 const levelMaterial = new CustomFresnel({
   colorMain: new Color(0.25, 0.08, 0.087),
@@ -12,7 +13,16 @@ const gloveMaterial = new CustomFresnel({
   colorFresnel: new Color(0.09, 0.0045, 0.08),
   power: 0.5,
 })
-const floorMaterial = new MeshPhongMaterial({ color: '#505090', shininess: 0.01 })
+const floorMaterial = new CustomNoise({
+  colorMain: new Color(0.4, 0.4, 0.6),
+  colorNoise: new Color(0.6, 0.6, 0.8),
+  contrast: 0.9,
+  gain: 1.0,
+  level: 0.05,
+  scale: 6,
+  octaves: 4,
+  seed: 7,
+})
 const testMaterial = new MeshBasicMaterial({ color: 'green', alphaTest: 2 })
 const tentMaterial = new CustomRays({
   color1: new Color(0.01, 0, 0.01),

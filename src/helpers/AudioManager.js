dc23c9e1b3c1ba11b3ece5a7a6fa80c1d8f68253
Peter Klinkon <info@kernel-image.net>
2024-12-09 18:18:32 -0500

    update AudioManager to play VO and random selection from by keyword

--- src/helpers/AudioManager.js
+++ src/helpers/AudioManager.js
@@ -1,46 +1,50 @@
-import { useSound } from 'use-sound';
-import { spritemap as SFXspritemap } from './bobo_sfx';
-import { spritemap as VOspritemap } from './bobo_vo';
+import { useSound } from 'use-sound'
+import { spritemap as SFXspritemap } from './bobo_sfx'
+import { spritemap as VOspritemap } from './bobo_vo'
 
-
+//console.log(SFXspritemap)
+//console.log(VOspritemap)
 
 const useSFX = (SERVER_PATH) => {
-    const [play] = useSound(`${SERVER_PATH}/audio/bobo_sfx.mp3`, {
-        sprite: SFXspritemap,
-        loop: false,
-    });
-    //console.log(spritemap)
-    return play;
+  const [play] = useSound(`${SERVER_PATH}/audio/bobo_sfx.mp3`, {
+    sprite: SFXspritemap,
+    loop: false,
+  })
+  //console.log(spritemap)
+  return play
 }
 
 const useMusic = (SERVER_PATH) => {
-    const [play] = useSound(`${SERVER_PATH}/audio/organ_grinder_01.mp3`, {
-        loop: true,
-        playbackRate: 0.8
-    });
-    return play;
+  const [play] = useSound(`${SERVER_PATH}/audio/organ_grinder_01.mp3`, {
+    loop: true,
+    playbackRate: 0.8,
+  })
+  return play
 }
 
 const useVO = (SERVER_PATH) => {
-    const [play] = useSound(`${SERVER_PATH}/audio/bobo_vo.mp3`, {
-        sprite: VOspritemap,
-        loop: false,
-    });
-    return { play, selectInactivityVO };
+  const [play] = useSound(`${SERVER_PATH}/audio/bobo_vo.mp3`, {
+    sprite: VOspritemap,
+    loop: false,
+    interrupt: true,
+    onEnd: () => {},
+  })
+  return play
 }
 
-const inactivityVO = Object.keys(VOspritemap).map((key) => {
-    if (key.includes('inactivity')) {
-        return key
+const getRandomID = (tag) => {
+  //console.log('getting vo id matching tag: ' + tag)
+  const voKeys = getVO(tag)
+  const idx = Math.floor(Math.random() * voKeys.length)
+  const id = voKeys[idx]
+  return id
+}
+
+const getVO = (tag) =>
+  Object.keys(VOspritemap).filter((key) => {
+    if (key.includes(tag)) {
+      return key
     }
-})
+  })
 
-const selectInactivityVO = () => {
-    const idx = Math.floor(Math.random() * inactivityVO.length)
-    const id = inactivityVO[idx]
-    return id
-}
-
-//TODO:  add VO asset and spritemap
-
-export { useSFX, useMusic, useVO }
+export { useSFX, useMusic, useVO, getRandomID }
